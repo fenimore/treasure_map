2f71b1d093a179d8330f3c684c712ac2a259f260
Fenimore Love <exorable.ludos@gmail.com>
2020-11-13 18:26:50 -0500

    Add proxy

--- treasure_map/app.py
+++ treasure_map/app.py
@@ -1,9 +1,7 @@
 from typing import Optional
 import os
 import logging
-from datetime import datetime
 
-import folium
 from flask import Flask, render_template, send_from_directory, request
 
 from stuff.search import Search
@@ -11,7 +9,6 @@ from stuff.client import StatefulClient
 from stuff.maps import Charter, NO_IMAGE
 from stuff.constants import (
     Category,
-    Area,
     Region,
 )
 
@@ -20,7 +17,7 @@ from treasure_map.city_list import CITIES
 
 def refine_city_name(location):
     """display User-friendly city name"""
-    if location == 'newyork': # does this have to capitalized
+    if location == 'newyork':  # does this have to capitalized
         loc = 'New York'
     elif location == 'washingtondc':
         loc = 'Washington D.C.'
@@ -30,34 +27,39 @@ def refine_city_name(location):
         loc = location
     return loc
 
+
 def get_things(
         location: str,
         quantity: int,
+        app_path: str,
         db_path: str,
         address: Optional[str] = None,
-        proxy: Optional[str] = None,
+        proxies: Optional[dict] = None,
 ):
-    "dict repr of the stuff for display on template"
+    """
+    Get a list of Stuff using the client, and save a map of them.
+
+    Return those Stuff objects in order to populate the view legend with a list of the things.
+    """
     client = StatefulClient.new(
         db_path=db_path,
         search=Search(region=Region(location), category=Category.free),
-        proxy=proxy,
+        proxies=proxies,
     )
     client.populate_db(enrich_inventory=True)
     stuffs = client.select_stuff(location, quantity)
     charter = Charter(stuffs=stuffs, city=location, zoom=12, address=address)
     charter.create_map()
-    folium_figure = charter.map.get_root()
 
-    map_path = os.path.join(app.root_path, 'templates', 'raw_map.html')
+    map_path = os.path.join(app_path, 'templates', 'raw_map.html')
     charter.save_map(
         map_path,
         css_children={
-            'bootstrap': os.path.join(app.root_path, 'static/css/style.css'),
+            'bootstrap': os.path.join(app_path, 'static/css/style.css'),
         }
     )
     charter.save_map(map_path=map_path)
-    things =[]
+    things = []
     for stuff in stuffs:
         image = stuff.image_urls[0] if stuff.image_urls else NO_IMAGE
         things.append({
@@ -74,15 +76,20 @@ def create_app(config={}):
     app = Flask(__name__)
 
     debug = config.get("debug", False)
-    proxy = config.get("proxy", None)
+    proxy_host = config.get("proxy", None)
     db_path = config.get("db_path", "sqlite:///")
+
+    proxies = {
+        "https": "https://" + proxy_host,
+        "http": "http://" + proxy_host,
+    } if proxy_host else None
 
     logging.info("Launching app with config:")
     logging.info(config)
 
     app.config.update(DEBUG=debug)
 
-    StatefulClient.new(db_path=db_path, proxy=proxy).setup()  # create db if doesn't exist
+    StatefulClient.new(db_path=db_path, proxies=proxies).setup()  # create db if doesn't exist
 
     @app.route('/favicon.ico')
     def favicon():
@@ -99,42 +106,40 @@ def create_app(config={}):
         """Render index."""
         StatefulClient.new(
             db_path=db_path,
-            proxy=proxy,
+            proxies=proxies,
         ).setup()
         return render_template('index.html')
-
 
     @app.route("/cities")
     def list_cities():
         """Display valid city names."""
         cities_list = '<table>'
-        cities_list +='<tr><th>User-Friendly Name</th><th>Valid for Url</th></tr>'
-        for key, value in CITIES.items() :
-            cities_list +='<tr>'
+        cities_list += '<tr><th>User-Friendly Name</th><th>Valid for Url</th></tr>'
+        for key, value in CITIES.items():
+            cities_list += '<tr>'
             cities_list += ('<td>' + str(key) + '</td><td>' + str(value) + '</td>')
-            cities_list +='</tr>'
+            cities_list += '</tr>'
         cities_list += '</table>'
         return cities_list
-
 
     @app.route('/<location>')
     def list_stuff(location):
         """Display listings"""
-        things = get_things(location, 25, db_path)
+        things = get_things(location, 25, app.root_path, db_path, proxies=proxies)
         refined_loc = refine_city_name(location)
         return render_template('view.html', things=things, location=location, rlocation=refined_loc)
 
     @app.route('/<location>/map')
     def show_map(location):
         """Display 10 items in given city, default"""
-        things = get_things(location, 25, db_path)
+        things = get_things(location, 25, app.root_path, db_path, proxies=proxies)
         location = refine_city_name(location)
 
         return render_template('map.html', location=location, things=things)
 
     @app.route('/<location>/map/<int:quantity>')
     def show_map_more(location, quantity):
-        things = get_things(location, quantity)
+        things = get_things(location, quantity, app.root_path, db_path, proxies=proxies)
         location = refine_city_name(location)
 
         return render_template('map.html', location=location, things=things)
@@ -145,7 +150,7 @@ def create_app(config={}):
             location = request.form['location']
             address = request.form['address']
 
-            things = get_things(location, 20, address=address)
+            things = get_things(location, 20, app.root_path, db_path, address=address, proxies=proxies)
             location = refine_city_name(location)
             return render_template('map.html', location=location, things=things)
 
