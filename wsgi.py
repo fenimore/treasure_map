c32ba8e6529931465edc8b98348328e7129994b0
Fenimore Love <exorable.ludos@gmail.com>
2020-11-12 22:41:29 -0500

    Add config

--- wsgi.py
+++ wsgi.py
@@ -1,23 +1,13 @@
 import sys
+import yaml
 
 from treasure_map.app import create_app
 
-remote_config = {
-    "db_path": "sqlite:////usr/local/share/treasure/treasure.db",
-    "proxy": None,
-}
 
-app = create_app(remote_config)
+config = yaml.safe_load(open("config.yml"))
 
+app = create_app(config)
 
 if __name__ == "__main__":
     port = int(sys.argv[1])
-
-    local_config = {
-        "db_path": "sqlite:///treasure.db",
-        "proxy": None,
-        "debug": True,
-    }
-
-    local_app = create_app(local_config)
-    local_app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
